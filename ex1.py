9a2e33354f81a206f93f252f8526ef788f9298ae
LeoGR22 <110871326+LeoGR22@users.noreply.github.com>
2022-10-19 22:17:35 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,21 @@
+A = [0]*6
+
+A[0] = 1
+A[1] = 0
+A[2] = 5
+A[3] = -2
+A[4] = -5
+A[5] = 7
+
+soma = A[0] + A[1] + A[5]
+print(soma)
+
+A[4] = 100
+print(A[4])
+
+print(A[0])
+print(A[1])
+print(A[2])
+print(A[3])
+print(A[4])
+print(A[5])
---
+++ ex3.py
@@ -0,0 +1,10 @@
+lista1 = []
+lista2 = []
+
+for i in range(1,11):
+    lista1.append(input("Acrescente um número"))
+
+for x in lista1:
+   lista2.append(int(x)*int(x))
+print(lista2)
+
